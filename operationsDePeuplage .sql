344de18763876cdc32df9324b13bc43e1b5cc076
merouane hany <merouane.hany@ynov.com>
2023-11-15 03:05:06 +0100

    finish commit

--- operationsDePeuplage .sql
+++ operationsDePeuplage .sql
@@ -1,49 +1,49 @@
 -- Insertion de données dans la table Client
-INSERT INTO Client (nom, prenom) VALUES
-('Doe', 'John'),
-('Smith', 'Alice'),
-('Johnson', 'Bob');
+INSERT INTO Client (id, nom, prenom) VALUES
+(1, 'Doe', 'John'),
+(2, 'Smith', 'Alice'),
+(3, 'Johnson', 'Bob');
 
 -- Insertion de données dans la table Admin
-INSERT INTO Admin (nom, prenom) VALUES
-('Admin1', 'PrenomAdmin1'),
-('Admin2', 'PrenomAdmin2');
+INSERT INTO Admin (id, nom, prenom) VALUES
+(1, 'Admin1', 'PrenomAdmin1'),
+(2, 'Admin2', 'PrenomAdmin2');
 
 -- Insertion de données dans la table SuperAdmin
-INSERT INTO SuperAdmin (nom, prenom) VALUES
-('SuperAdmin1', 'PrenomSuperAdmin1'),
-('SuperAdmin2', 'PrenomSuperAdmin2');
+INSERT INTO SuperAdmin (id, nom, prenom) VALUES
+(1, 'SuperAdmin1', 'PrenomSuperAdmin1'),
+(2, 'SuperAdmin2', 'PrenomSuperAdmin2');
 
 -- Insertion de données dans la table Vendeur
-INSERT INTO Vendeur (nom, prenom) VALUES
-('Vendeur1', 'PrenomVendeur1'),
-('Vendeur2', 'PrenomVendeur2');
+INSERT INTO Vendeur (id, nom, prenom) VALUES
+(1, 'Vendeur1', 'PrenomVendeur1'),
+(2, 'Vendeur2', 'PrenomVendeur2');
 
 -- Insertion de données dans la table Boutique
-INSERT INTO Boutique (nom, vendeur_id) VALUES
-('Boutique1', 1),
-('Boutique2', 2);
+INSERT INTO Boutique (id, nom, vendeur_id) VALUES
+(1, 'Boutique1', 1),
+(2, 'Boutique2', 2);
 
 -- Insertion de données dans la table Employe
-INSERT INTO Employe (nom, prenom, boutique_id) VALUES
-('Employe1', 'PrenomEmploye1', 1),
-('Employe2', 'PrenomEmploye2', 2);
+INSERT INTO Employe (id, nom, prenom, boutique_id) VALUES
+(1, 'Employe1', 'PrenomEmploye1', 1),
+(2, 'Employe2', 'PrenomEmploye2', 2);
 
 -- Insertion de données dans la table Produit
-INSERT INTO Produit (nom, description, boutique_id, prix) VALUES
-('Produit1', 'DescriptionProduit1', 1, 19.99),
-('Produit2', 'DescriptionProduit2', 1, 29.99),
-('Produit3', 'DescriptionProduit3', 2, 24.99);
+INSERT INTO Produit (id, nom, description, boutique_id, prix) VALUES
+(1, 'Produit1', 'DescriptionProduit1', 1, 19.99),
+(2, 'Produit2', 'DescriptionProduit2', 1, 29.99),
+(3, 'Produit3', 'DescriptionProduit3', 2, 24.99);
 
 -- Insertion de données dans la table Commande
-INSERT INTO Commande (date, client_id) VALUES
-('2023-01-01', 1),
-('2023-02-15', 2),
-('2023-03-20', 3);
+INSERT INTO Commande (id, date, client_id) VALUES
+(1, '2023-01-01', 1),
+(2, '2023-02-15', 2),
+(3, '2023-03-20', 3);
 
 -- Insertion de données dans la table ProduitCommande
-INSERT INTO ProduitCommande (produit_id, commande_id) VALUES
-(1, 1),
-(2, 1),
-(2, 2),
-(3, 3);
+INSERT INTO ProduitCommande (id, produit_id, commande_id) VALUES
+(1, 1, 1),
+(2, 2, 1),
+(3, 2, 2),
+(4, 3, 3);
