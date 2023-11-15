5ff9506c55f15c48b1063d5ceff51ca251d1d208
merouane hany <merouane.hany@ynov.com>
2023-11-15 01:24:52 +0100

    4 commit

--- e-commerce.sql
+++ e-commerce.sql
@@ -6,28 +6,28 @@ USE Ecommerce;
 
 -- Table pour stocker les informations des Clients
 CREATE TABLE `Client` (
-  `id` INT PRIMARY KEY,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `nom` VARCHAR(255),
   `prenom` VARCHAR(255)
 );
 
 -- Table pour stocker les informations des Administrateurs
 CREATE TABLE `Admin` (
-  `id` INT PRIMARY KEY,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `nom` VARCHAR(255),
   `prenom` VARCHAR(255)
 );
 
 -- Table pour stocker les informations des Super Administrateurs
 CREATE TABLE `SuperAdmin` (
-  `id` INT PRIMARY KEY,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `nom` VARCHAR(255),
   `prenom` VARCHAR(255)
 );
 
 -- Table pour stocker les informations des Commandes
 CREATE TABLE `Commande` (
-  `id` INT PRIMARY KEY,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `date` DATE,
   `client_id` INT,
   FOREIGN KEY (`client_id`) REFERENCES `Client` (`id`) ON UPDATE CASCADE -- Mise à jour en cascade
@@ -35,14 +35,14 @@ CREATE TABLE `Commande` (
 
 -- Table pour stocker les informations des Vendeurs
 CREATE TABLE `Vendeur` (
-  `id` INT PRIMARY KEY,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `nom` VARCHAR(255),
   `prenom` VARCHAR(255)
 );
 
 -- Table pour stocker les informations des Boutiques
 CREATE TABLE `Boutique` (
-  `id` INT PRIMARY KEY,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `nom` VARCHAR(255),
   `vendeur_id` INT,
   FOREIGN KEY (`vendeur_id`) REFERENCES `Vendeur` (`id`) ON UPDATE CASCADE -- Mise à jour en cascade
@@ -50,7 +50,7 @@ CREATE TABLE `Boutique` (
 
 -- Table pour stocker les informations des Employés
 CREATE TABLE `Employe` (
-  `id` INT PRIMARY KEY,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `nom` VARCHAR(255),
   `prenom` VARCHAR(255),
   `boutique_id` INT,
@@ -59,16 +59,17 @@ CREATE TABLE `Employe` (
 
 -- Table pour stocker les informations des Produits
 CREATE TABLE `Produit` (
-  `id` INT PRIMARY KEY,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `nom` VARCHAR(255),
   `description` TEXT,
   `boutique_id` INT,
+  `prix` DECIMAL(10, 2), -- Ajout de la colonne prix
   FOREIGN KEY (`boutique_id`) REFERENCES `Boutique` (`id`) ON UPDATE CASCADE -- Mise à jour en cascade
 );
 
 -- Table pivot pour représenter la relation many-to-many entre Produits et Commandes
 CREATE TABLE `ProduitCommande` (
-  `id` INT PRIMARY KEY,
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
   `produit_id` INT,
   `commande_id` INT,
   FOREIGN KEY (`produit_id`) REFERENCES `Produit` (`id`) ON UPDATE CASCADE, -- Mise à jour en cascade
--- operations.sql
+++ operations.sql
@@ -14,13 +14,13 @@ INSERT INTO ProduitCommande (id, produit_id, commande_id) VALUES (1, 1, 1);
 -- Opérations en tant que Vendeur
 
 -- Créer une nouvelle boutique en tant que vendeur
-INSERT INTO Boutique (nom, vendeur_id) VALUES ('NouvelleBoutique', 1);
+INSERT INTO Boutique (id,nom, vendeur_id) VALUES ('NouvelleBoutique', 1);
 
 -- Ajouter un employé à votre boutique
-INSERT INTO Employe (nom, prenom, boutique_id) VALUES ('NouvelEmploye', 'PrenomNouvelEmploye', 2);
+INSERT INTO Employe (id,nom, prenom, boutique_id) VALUES ('NouvelEmploye', 'PrenomNouvelEmploye', 2);
 
 -- Ajouter un produit à votre boutique
-INSERT INTO Produit (nom, description, boutique_id) VALUES ('NouveauProduit', 'DescriptionNouveauProduit', 2);
+INSERT INTO Produit (id,nom, description, boutique_id) VALUES ('NouveauProduit', 'DescriptionNouveauProduit', 2);
 
 -- Consulter les commandes liées à votre boutique
 SELECT * FROM Commande WHERE client_id IN (SELECT id FROM Client WHERE id = 1) AND id IN (SELECT commande_id FROM ProduitCommande WHERE produit_id IN (SELECT id FROM Produit WHERE boutique_id = 2));
